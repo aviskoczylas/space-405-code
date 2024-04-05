d8c1aad7fabc8147972c8c3b9b8564e814851688
pickleemperor <157718132+pickleemperor@users.noreply.github.com>
2024-04-05 14:53:52 -0400

    Update juno_data_processing.py

--- juno_data_processing.py
+++ juno_data_processing.py
@@ -1,5 +1,5 @@
 import numpy as np, h5py, matplotlib.pyplot as plt
-filepath='C:/Users/abrah/Downloads/Juno_MWR-Antenna_Temperatures-Swath-Perijove_19.h5'
+filepath='C:/Users/abrah/OneDrive/Desktop/space 405 project/Juno_MWR-Antenna_Temperatures-Swath-Perijove_19.h5'
 f = h5py.File(filepath, 'r')
 
 #creates 3 lists containing the members of each group
@@ -8,11 +8,11 @@ list_s1=[]
 list_s2=[]
 for group_name in f:
     group = f[group_name]
-    print("Group:", group_name)
+    #print("Group:", group_name)
 
     for member_name in group:
         member = group[member_name]
-        print("Member:", member_name)
+        #print("Member:", member_name)
         if(group_name=='s0'):
             list_s0.append(member)
         elif(group_name=='s1'):
@@ -30,26 +30,32 @@ list_s2 = [np.array(member) for member in list_s2]
 
 time_axis = np.arange(0, len(list_s0[0]))
 
+#uncomment any specific plot you want to see!
+
+'''
 #plot latitude vs time
 plt.plot(time_axis, list_s0[0])
 plt.xlabel('Time')
 plt.ylabel('Latitude')
 plt.title('Latitude of Spacecraft Over Time')
 plt.show()
+'''
 
+'''
 #plot longitude vs time
 plt.plot(time_axis, list_s0[1])
 plt.xlabel('Time')
 plt.ylabel('Longitude')
 plt.title('Longitude of Spacecraft Over Time')
 plt.show()
+'''
 
+'''
 #plot parametric curve, or latitude + longitude vs time
 plt.plot(list_s0[0], list_s0[1])
 plt.xlabel('Latitude')
 plt.ylabel('Longitude')
 plt.title('Path of Spacecraft Over Time')
-
 #add time points
 x=[]
 y=[]
@@ -62,17 +68,36 @@ plt.scatter(x, y)
 for i, (x_val,y_val,t_val) in enumerate(zip(x,y,t)):
     plt.text(x_val, y_val, t_val)
 plt.show()
+'''
+#the code explanation file has been updated until here
 
+x1=[]
+y1=[]
+angle1=[]
+tempch6=[]
+for row_index, row in enumerate(list_s2[0]):
+    for col_index, element in enumerate(row):
+        angle1.append(element)
+        x1.append(list_s2[7][row_index, col_index])
+        y1.append(list_s2[6][row_index, col_index])
+        tempch6.append(list_s2[5][row_index, col_index])
+
+'''
 #plot local zenith angle vs location
-plt.pcolor(list_s1[0])
-plt.xlabel('X axis (unknown units atm)')
-plt.ylabel('Y axis (unknown units atm)')
+plt.scatter(x1,y1,c=angle1, cmap='viridis') 
+plt.colorbar() 
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
 plt.title('Local Zenith Angle')
 plt.show()
+'''
 
-#plot temperature vs location
-plt.pcolor(list_s1[1])
-plt.xlabel('X axis (unknown units atm)')
-plt.ylabel('Y axis (unknown units atm)')
-plt.title('Channel 1 Temperature')
+'''
+#plot channel 6 temperature vs location
+plt.scatter(x1,y1,c=tempch6, cmap='viridis') 
+plt.colorbar() 
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Temperature in channel 6')
 plt.show()
+'''
